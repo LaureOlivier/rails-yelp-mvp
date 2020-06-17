3fef32cc63eba4d32ded86ba555b4f4613d42e66
Laure Olivier <laurelacombeolivier@gmail.com>
2020-06-17 15:38:17 +0100

    ouaich exo ok

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+# Added gems from LW setup
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+
+# Added the gem simple-form to get bootstrap-compatible forms with a lighter syntax
+gem 'simple_form'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+love_sushi = { name: "Love Sushi", address: "Praça Olegario Mariano, 4, Lisboa 1170 - 278", category: "japanese" }
+madame_gen = { name: "Madame Gen", address: "14 rue des Petites Ecuries, 75010 Paris", category: "french" }
+vive_la_frite = { name: "Vive la frite", address: "113 allee des Hospices, Uccle", category: "belgian" }
+
+[ dishoom, pizza_east, love_sushi, madame_gen, vive_la_frite ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
